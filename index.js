2827c54803b6a3fed262ee6a8d950c845d942335
evilive3000 <evilive3000@gmail.com>
2016-07-28 11:55:21 +0300

    избавился от разбиения пачки айдишников на чанки, теперь обрабатываю список последовательно в limit-потоков.

--- index.js
+++ index.js
@@ -10,39 +10,46 @@ const tidido = require('./providers/tidido');
 const async = require('async');
 const Chalk = require('chalk');
 
-const colors = ['red', 'green', 'blue', 'yellow', 'magenta', 'cyan', 'white', 'gray', 'black'];
+const colors = ['red', 'green', 'blue', 'yellow', 'magenta', 'cyan', /*'white', */'gray'/*, 'black'*/];
 
-function processChunk(provider, ids, callback) {
+/**
+ *
+ * @param provider
+ * @param id
+ * @param pid
+ * @param callback
+ */
+function processSong(provider, id, pid, callback) {
   const color = colors.shift() || 'bgRed';
   const cstr = Chalk[color];
-  let counter = ids.length;
-  const writeln = str => process.stdout.write(cstr(`[${counter}] ${str}\n`));
+  const writeln = str => process.stdout.write(cstr(`[${pid}] ${str}\n`));
 
   co(function*() {
-    for (const id of ids) {
-      const _id = id._id.toString();
+    const _id = id._id.toString();
 
-      writeln(`fetching: ${_id}`);
-      const song = yield tidido.fetchOne(_id);
-      song._id = id._id;
-      const title = `${song.artist} - ${song.title}`;
+    writeln(`fetching: ${_id}`);
+    const song = yield tidido.fetchOne(_id);
+    song._id = id._id;
+    const title = `${song.artist} - ${song.title}`;
 
-      writeln(`siblings: ${title}`);
-      const siblings = yield getSiblings(provider, song, config);
+    writeln(`siblings: ${title}`);
+    const siblings = yield getSiblings(provider, song, config);
 
-      const mse = siblings.map(s => s.mse.val.toFixed(2));
-      mse.length === 0
-        ? writeln(cstr.bold(`"${title}" [no siblings]`))
-        : writeln(cstr.bold(`"${title}" has siblings: [${mse.join(", ")}]`));
+    const mse = siblings.map(s => s.mse.val.toFixed(2));
+    mse.length === 0
+      ? writeln(`"${title}" ${cstr.bold("[no siblings]")}`)
+      : writeln(`"${title}" has siblings: ${cstr.bold(`[${mse.join(", ")}]`)}`);
 
-      yield store(provider, song, siblings);
-      counter--;
-    }
+    yield store(provider, song, siblings);
     colors.push(color);
   }).then(callback)
     .catch(callback);
 }
 
+/**
+ *
+ * @returns {String}
+ */
 function getProvider() {
   const name = process.argv[2];
   if (["youtube"].includes(name)) {
@@ -51,13 +58,15 @@ function getProvider() {
   throw new Error(`Provider ${name} not defined.`);
 }
 
+/**
+ *
+ */
 co(function*() {
   const provider = getProvider();
   const limit = parseInt(process.argv[3]) || 3;
-  const ids = yield ws(provider, 4000, 3);
-  const chunks = _.chunk(ids, Math.ceil(ids.length / limit));
+  const ids = yield ws(provider, 1000, 3);
 
-  async.each(chunks, _.partial(processChunk, provider), err => {
+  async.eachOfLimit(ids, limit, _.partial(processSong, provider), err => {
     err && console.log(err);
     console.log('done');
   })
